168e6578c9c86e01a4b6a23628e9094a96f4605c
sandeeptottadi <76050316+sandeeptottadi@users.noreply.github.com>
2024-02-03 08:00:35 -0800

    Time: 63 ms (77.78%) | Memory: 49.4 MB (9.72%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumAfterPartitioning = function(arr, k) {
+        const N = arr.length;
+        const K = k + 1;
+
+        const dp = Array(K).fill(0);
+
+        for (let start = N - 1; start >= 0; start--) {
+            let currMax = 0;
+            const end = Math.min(N, start + k);
+
+            for (let i = start; i < end; i++) {
+                currMax = Math.max(currMax, arr[i]);
+                dp[start % K] = Math.max(dp[start % K], dp[(i + 1) % K] + currMax * (i - start + 1));
+            }
+        }
+        return dp[0];
+};
