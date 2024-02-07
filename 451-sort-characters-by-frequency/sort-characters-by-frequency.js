fb1fa378b255e10af93796f69c2f9eb55a320233
sandeeptottadi <76050316+sandeeptottadi@users.noreply.github.com>
2024-02-07 08:27:06 -0800

    Time: 71 ms (68.72%) | Memory: 53.9 MB (29.22%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    const counter = new Map();
+    for (const char of s) {
+        counter.set(char, (counter.get(char) || 0) + 1);
+    }
+    
+    const pq = Array.from(counter.entries());
+    pq.sort((a, b) => b[1] - a[1]);
+    
+    let result = '';
+    for (const [char, freq] of pq) {
+        result += char.repeat(freq);
+    }
+    
+    return result;
+};
