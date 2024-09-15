210e8813a6cfb3c3df9a594ab93b2886144cb102
sandeeptottadi <76050316+sandeeptottadi@users.noreply.github.com>
2024-09-15 04:51:45 -0700

    Time: 127 ms (22.12%) | Memory: 57.4 MB (15.19%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if(!s) return 0;
+    let res = 1;
+    if(s.length === 1) return 1;
+    let i = 0, j = 1;
+    let dict = {};
+    dict[s[0]] = 0;
+    while(j < s.length) {
+        if(dict[s[j]] !== undefined) {
+            if(j-i > res) res = j-i;
+            if(dict[s[j]] + 1 > i) i = dict[s[j]]+1;
+            dict[s[j]] = j;
+        } else
+            dict[s[j]] = j;
+        j++;
+    }
+    console.log(res, i, j)
+    return j-i > res? j-i : res;
+};
