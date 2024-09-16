bfbd13f2a76dd81b8651ad56da4a1036c0fdc162
sandeeptottadi <76050316+sandeeptottadi@users.noreply.github.com>
2024-09-16 01:34:06 -0700

    Time: 86 ms (12.52%) | Memory: 53.5 MB (31.04%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,10 +3,9 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    let l = 0, r = n, mid;
+    let l = 0, r = Math.sqrt(n)+1, mid;
     while(l <= r) {
         mid = Math.floor((r+l)/2);
-        console.log(mid)
         if(Math.pow(2, mid) === n) return true;
         if(Math.pow(2, mid) > n) r = mid-1;
         else l = mid+1;
